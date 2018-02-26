ee3ebb8adc7538f3112f3b18e994bc2367985032
guilherme.natan <guilherme.natan.barbosa@gmail.com>
2018-02-25 21:15:08 -0300

    Melhoria no layout para visualizar empresas.

--- app/src/main/java/android/guiacomercial/VisualizarEmpresaActivity.java
+++ app/src/main/java/android/guiacomercial/VisualizarEmpresaActivity.java
@@ -1,10 +1,9 @@
 package android.guiacomercial;
 
-import android.guiacomercial.converters.Converter;
-import android.guiacomercial.model.Empresa;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
 
 /**
  * Created by guilherme.natan on 17/02/2018.
@@ -16,19 +15,26 @@ public class  VisualizarEmpresaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_empresa);
-        instanciarElementos();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_layout);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+//        instanciarElementos();
     }
 
     public void instanciarElementos()
     {
-        Converter<Empresa> converter = new Converter<>();
-        Empresa empresa = converter.converterStringEntidade(getIntent().getExtras()
-                .getString(Empresa.key), Empresa.class);
-        TextView nome = findViewById(R.id.txt_visualizar_nomeempresa);
-        TextView nota = findViewById(R.id.txt_visualizar_notaempresa);
-        TextView descricao = findViewById(R.id.txt_descricaoempresa);
-        nome.setText(empresa.getNome());
-        nota.setText("Nota: "+empresa.getNota().toString());
-        descricao.setText(empresa.getDescricao());
+//        Converter<Empresa> converter = new Converter<>();
+//        Empresa empresa = converter.converterStringEntidade(getIntent().getExtras()
+//                .getString(Empresa.key), Empresa.class);
+//        TextView nome = findViewById(R.id.txt_visualizar_nomeempresa);
+//        TextView nota = findViewById(R.id.txt_visualizar_notaempresa);
+//        TextView descricao = findViewById(R.id.txt_descricaoempresa);
+//        nome.setText(empresa.getNome());
+//        nota.setText("Nota: "+empresa.getNota().toString());
+//        descricao.setText(empresa.getDescricao());
     }
 }
